8124dd9b6c65373c5af209201cc4d2f526292707
smpn1kajen <buturiyah@gmail.com>
2026-09-25 16:14:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const STATE = { siswa:[], absensi:[], pelanggaran:[], konseling:[], kolaborasi:[
    situs ini (lihat PANDUAN-UPDATE.md), supaya guru mapel tidak perlu tahu atau
    menempel URL Apps Script sama sekali. Kalau dikosongkan, layar Admin BK tetap
    bisa mengisi URL secara manual seperti sebelumnya (mode lama tidak rusak). */
-const DEFAULT_API_URL = 'https://script.google.com/macros/s/AKfycbzt0Q8LiLvBj3hXNasmU8GlvkSzsh3cBtdYVEp_7dQBvWDv-X2y8GRd85KASRLyftPT/exec';
+const DEFAULT_API_URL = 'https://script.google.com/macros/s/AKfycbyKv-bjN08P3n_56xmNBkMniw-pUJGdZuML-XcR-isKAoa00CigVHWKnp-OSsFZwOE/exec';
 
 let API_URL = localStorage.getItem('bk_api_url') || DEFAULT_API_URL;
 let API_TOKEN = localStorage.getItem('bk_api_token') || '';
